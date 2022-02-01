47f54f73ab3840d2aa1eff9cd304eb02a18e2a8f
Hui Wu <wh241300@mail.ustc.edu.cn>
2022-02-01 16:41:15 +0800

    Update experiment_augrSfm120k.sh

--- experiment_augrSfm120k.sh
+++ experiment_augrSfm120k.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 python -m torch.distributed.launch --nproc_per_node=4 --use_env train.py \
 --test_name TopkSIM_SFMAUG \
---input_dim 2048 \
 --embed_dim 768 \
 --num_heads 12 \
 --depth 2 \
 --topk 512 \
---test_topk 1024 \
 --sim_len 512 \
 --batch_size 256 \
 --num_workers 4 \
@@ -16,7 +14,7 @@ python -m torch.distributed.launch --nproc_per_node=4 --use_env train.py \
 --drop_rate 0.1 \
 --drop_path_rate 0.1 \
 --attn_drop_rate 0.1 \
---dataset_names [gl18-tl-resnet50-gem-w,gl18-tl-resnet101-gem-w,gl18-tl-resnet152-gem-w] \
+--dataset_names gl18-tl-resnet50-gem-w gl18-tl-resnet101-gem-w gl18-tl-resnet152-gem-w \
 --device cuda \
 --num_epochs 100 \
 --stop_at_epoch 100 \
